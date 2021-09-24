70809f42ec528d0a5b9d24c4b629da9c55f7221e
Omid Ghozatlou <46296636+omid-ghozatlou@users.noreply.github.com>
2021-09-24 14:03:10 +0200

    Update Stacked12Bands.py

--- Stacked12Bands.py
+++ Stacked12Bands.py
@@ -5,30 +5,10 @@ Created on Tue Feb 16 14:55:32 2021
 @author: CEOSpaceTech
 """
 
-
 import tifffile
-# from PIL import Image
 import os, os.path
 import numpy as np
 #----------------------------- full 12 bands ---------------------------------------
-# path = "D:/Omid/UPB/Datasets/Paris/128x128 batches/Full_Bands_128/S2B_MSIL2A_20200319T105649_N0214_R094_T31UDQ_20200319T142847.SAFE"
-# path_stacked = path+'/Stacked'
-# valid_images = [".jpg",".tif",".png",".tga"]
-# imgs_B2=[]
-# path_all = path+'/all' #directory of all patches 
-# for f in os.listdir(path_all):
-#     ext = os.path.splitext(f)[1]
-#     if ext.lower() not in valid_images:
-#         continue
-#     imgs_B2.append(tifffile.imread(os.path.join(path_all,f)))
-
-
-# n=1312   #number of patches for each band 
-# for i in range(n):
-#     img = np.dstack((imgs_B2[i],imgs_B2[i+3*n],imgs_B2[i+4*n],imgs_B2[i+5*n],imgs_B2[i+6*n],imgs_B2[i+7*n],imgs_B2[i+8*n],imgs_B2[i+9*n],imgs_B2[i+10*n],imgs_B2[i+11*n],imgs_B2[i+n],imgs_B2[i+2*n]))
-#     tifffile.imwrite(path_stacked+"\S2B_MSIL2A_20200319T105649_RGB_"+str(i)+".tif", img, planarconfig='CONTIG')
-
-#----------------------------------- 10 bands 10m & 20m (subset normalization 8000) --------------------------------------
 path = "D:/Sciebo/ESR14/cloud removal/dataset/S2B_MSIL2A_20200319T105649_N0214_R094_T31UDQ_20200319T142847.SAFE"
 path_stacked = path+'/stacked'
 valid_images = [".jpg",".tif",".png",".tga"]
@@ -45,5 +25,6 @@ n=1312
 for i in range(n):
     img = np.dstack((imgs_B2[i+3*n],imgs_B2[i+4*n],imgs_B2[i+5*n],imgs_B2[i+6*n],imgs_B2[i+7*n],imgs_B2[i+8*n],imgs_B2[i+9*n],imgs_B2[i+10*n],imgs_B2[i+n],imgs_B2[i+2*n]))
     tifffile.imwrite(path_stacked+"\S2B_MSIL2A_20201124T110359_RGB_"+str(i)+".tif", img, planarconfig='CONTIG')
+
 
 
