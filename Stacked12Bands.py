c37cd64de56d22a85d77286063c655f02c2260d9
Omid Ghozatlou <46296636+omid-ghozatlou@users.noreply.github.com>
2021-09-28 10:15:37 +0300

    Update Stacked12Bands.py
    add some commnets

--- Stacked12Bands.py
+++ Stacked12Bands.py
@@ -9,7 +9,7 @@ import tifffile
 import os, os.path
 import numpy as np
 #----------------------------- full 12 bands ---------------------------------------
-path = "D:/Sciebo/ESR14/cloud removal/dataset/S2B_MSIL2A_20200319T105649_N0214_R094_T31UDQ_20200319T142847.SAFE"
+path = "D:/Sciebo/ESR14/cloud removal/dataset/S2B_MSIL2A_20200319T105649_N0214_R094_T31UDQ_20200319T142847.SAFE" # directory of patches includes 12 bands
 path_stacked = path+'/stacked'
 valid_images = [".jpg",".tif",".png",".tga"]
 imgs_B2=[]
@@ -21,7 +21,7 @@ for f in os.listdir(path_all):
     imgs_B2.append(tifffile.imread(os.path.join(path_all,f)))
 
 
-n=1312    
+n=1312 # number of patches of each band   
 for i in range(n):
     img = np.dstack((imgs_B2[i+3*n],imgs_B2[i+4*n],imgs_B2[i+5*n],imgs_B2[i+6*n],imgs_B2[i+7*n],imgs_B2[i+8*n],imgs_B2[i+9*n],imgs_B2[i+10*n],imgs_B2[i+n],imgs_B2[i+2*n]))
     tifffile.imwrite(path_stacked+"\S2B_MSIL2A_20201124T110359_RGB_"+str(i)+".tif", img, planarconfig='CONTIG')
